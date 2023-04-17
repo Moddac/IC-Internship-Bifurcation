a0043720e4260b6431252d9548f2c106fde7f735
Henri Pinsolle <henri.pinsolle@gmail.com>
2023-04-17 16:15:25 +0200

    Init

---
+++ Brownian_motion.py
@@ -0,0 +1,73 @@
+"""
+Creation of a Brownian walk & plot
+"""
+
+import numpy as np 
+from numpy.random import normal
+import matplotlib.pyplot as plt
+from matplotlib.animation import PillowWriter
+def simul_walk(T,N):
+    """
+    Simulate the Brownian walk, T is the final time & N is the number of steps 
+    """
+    #-----Init----
+    h = T/N
+    ts = np.arange(0,T,h)
+    W = 0
+    Ws = []
+
+    for t in ts:
+        δ = normal(0,h)
+        W = W + δ 
+        Ws.append(W)
+
+    return np.array(Ws)
+
+def plot_walk(T,N):
+    """
+    plotting the Brownian walk 
+    """
+    h = T/N
+    ts = np.arange(0,T,h)
+    Ws = simul_walk(T,N)
+
+    plt.plot(ts,Ws)
+    plt.plot((0,T),(0,0))
+    plt.show()
+    return Ws
+
+def anim_walk(T,N,number_walks=1):
+    """
+    Animating the walk 
+    """
+    #-----Init----
+    h = T/N
+    ts = np.arange(0,T,h)
+    W_last = np.zeros(number_walks)
+    W_new = np.zeros(number_walks)
+
+    fig, ax = plt.subplots()
+    ax.set_xlim(0,T)
+    ax.set_ylim(-3,3)
+    ax.plot((0,T),(0,0),color='orange')
+    ax.set_title(f'Brownian walks for T={T} and N={N}')
+    writer = PillowWriter(fps=15)
+
+    with writer.saving(fig, 'brownian_walk.gif', dpi=100):
+        for i,t in enumerate(ts[:-1]):
+            for n in range(number_walks):
+                δ = normal(0,h)
+                W_new[n] = W_last[n] + δ  
+                ax.plot(ts[i:i+2],[W_last[n],W_new[n]],color=f'{n/number_walks}')
+                W_last[n] = W_new[n]
+
+            writer.grab_frame()
+    
+    return        
+
+if __name__=='__main__':
+    T = 10
+    N = 300
+    # plot_walk(T,N)
+    anim_walk(T,N,number_walks=3)
+
---
+++ Examples_SDE.py
@@ -0,0 +1,4 @@
+"""
+Showing how to use 'Stochastic_Integral.py' by solving multiple equations with known solutions
+"""
+
